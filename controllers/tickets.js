bc40633feae40c016ab1e5adb95820347e51023f
maliqtaylor <maliq.taylor@icloud.com>
2020-12-09 00:41:05 -0500

    display flight details

---
+++ controllers/tickets.js
@@ -0,0 +1,15 @@
+const Flight = require('../models/flight')
+
+module.exports = {
+  create
+}
+
+function create(req, res) {
+  Flight.findById(req.params.id,
+    (err, flight) => {
+      flight.tickets.push(req.body)
+      flight.save((err) => {
+        res.redirect(`/flights/${flight._id}`)
+      })
+    })
+}
